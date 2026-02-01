f5d70cabc95abc5f714ea2aad25a6e3bd2b7e82c
Pradhum80 <kumarroney154@gmail.com>
2026-02-01 10:09:28 +0530

    First : Program

---
+++ firstProgram.py
@@ -0,0 +1,9 @@
+print("Hello World !.")
+print ("My Name is Pradhum Kumar")
+
+# Same line Print...
+print("Hello World!. ", "My name is Pradhum Kumar.") #using Coomma same Line..
+
+# digit Print .
+print(17)
+print(47)
