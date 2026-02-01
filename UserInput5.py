3955a7291e2319c492daf0eeae52ac19142a0cf4
Pradhum80 <kumarroney154@gmail.com>
2026-02-01 11:29:24 +0530

    Users : Input

---
+++ UserInput5.py
@@ -0,0 +1,16 @@
+# Input  in Python..
+# input() Statement is used to accept values (Using Keyboard) from user
+
+# String input.
+name = input("name : ")
+print(name)
+
+# int input
+age = int(input("age : "))
+# print(age)
+
+#float input
+price = float(input("price : "))
+
+#
+print("My name is: ", name, "and i am", age, "years old")
