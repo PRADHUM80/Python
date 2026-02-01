f9eaec8ee0bbd2f0120fc5267b2125e150bffe51
Pradhum80 <kumarroney154@gmail.com>
2026-02-01 11:16:13 +0530

    Expression : Execution

---
+++ ExpressionExecution4.py
@@ -0,0 +1,29 @@
+# String & Numeric values can Operates together with *.
+
+A, B =2, 3
+Txt = "@ "
+print(2 * Txt * 3)
+
+# String & String can Operate with +..
+A, B = "2",3
+Txt = "@"
+print((A + Txt)*B)
+
+#floor gives closet integer, which ids lesser than or equal to the float value Result of (A // B) is same as floor(A/B)
+A, B = 12, 5
+C = A // B
+print(C)
+
+D, E = -12, 5
+F = D // E
+print(F)
+
+# Reminder is negative when deniminator is negative.
+A,B = -5, 2
+C = A%B
+print(C)
+
+A,B= 5, -2
+C= A%B
+print(C)
+
---
+++ Sum3.py
@@ -0,0 +1,8 @@
+a = 5
+b = 5
+sum = a + b
+print(sum)  
+
+# Punctuators : () {} @ [] #  -+ -= *= /= etc..
+
+# Types :-   
