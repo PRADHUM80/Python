e712e9ac0e68d510e9c117d186f955e95b6d376f
Pradhum80 <kumarroney154@gmail.com>
2026-02-01 10:39:27 +0530

    Variables : Type

---
+++ Variables2.py
@@ -0,0 +1,26 @@
+name = "Pradhum"
+age = 21
+price = 25.99
+
+# = means value assigne..
+
+print ("my name is : " , name)
+print("my age is : " , age)
+print(price)
+
+#
+age2 = age
+print(age2)
+
+
+ # Type.. DataTypes..
+print(type(name))
+print(type(age))
+print(type(price))
+
+#Data Tyoe... None Data Type...
+a = None
+print(type(a))
+
+#Keywords .. Keywords are reserved words in python.
+# and , as , assert , break, class, continue, def, del, elif, else, etc..
