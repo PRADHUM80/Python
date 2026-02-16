eca8bf1a45291f726e461165a3504fa12be82a2b
Pradhum80 <kumarroney154@gmail.com>
2026-02-16 15:37:48 +0530

    Type : Conversion

---
+++ TypeConversion8.py
@@ -0,0 +1,19 @@
+# one value variable to change another variable value...
+
+# Type conversion :- Automatically .  Type Casting:- Manualy
+
+a = 2
+b = 4.25
+
+sum = a + b  # int convert float.2.0 + 4.25
+print(sum)  
+
+# Type Casting :-  int (value)  float(value)
+
+c = int("2")
+d = 4.25
+print(c + d)
+
+
+
+
