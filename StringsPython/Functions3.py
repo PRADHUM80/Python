9bc2b6fb907d1ae771baa9878ddd292374284bec
Pradhum80 <kumarroney154@gmail.com>
2026-02-16 17:21:44 +0530

    Functions : String

---
+++ StringsPython/Functions3.py
@@ -0,0 +1,25 @@
+# String Function..
+
+# returns true if String ends with substr
+str = "i am  studying python from ApnaCollege"
+print(str.endswith("ege"))
+
+# Capitalizes 1st char.
+print(str.capitalize())
+
+# Replace 
+print(str.replace("o", "@"))
+print(str.replace("ApnaCollege", "Kodnest"))
+
+# find .. returns 1st index of 1st occurrer..
+print(str.find("o"))
+# print(str.find(Q))  # -1 doesnot exists.
+
+
+# Count .. Counts the occurrence of substr..
+print(str.count("from"))
+print(str.count("o"))
+
+
+
+# String. more function show..
