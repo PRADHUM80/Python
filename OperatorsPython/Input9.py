f1680fdddc3f729d883bf96d7b8cc1a7e8499348
Pradhum80 <kumarroney154@gmail.com>
2026-02-16 15:53:37 +0530

    User : Input

---
+++ OperatorsPython/Input9.py
@@ -0,0 +1,21 @@
+# input in Python
+
+# input() Statement is used to accept values (using keybord) from user...
+
+# input("Enter your name: ") # direct 
+# create variable
+# name = input("Enter your name: ")
+# print("Hi ", name)
+
+
+# val = input("enter some value : ")
+# print(type(val), val) # "25", "99.99" converted
+
+#
+name = input("enter name : ")
+age = int(input("enter age : "))
+marks = float(input("enter marks: "))
+
+print("Name: ", name)
+print("age: ", age)
+print("marks : ", marks)
