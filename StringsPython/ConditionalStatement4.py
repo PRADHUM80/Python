94bab3de4883bbf08e5b67afadef65df4e4340da
Pradhum80 <kumarroney154@gmail.com>
2026-02-16 17:54:58 +0530

    Loop : String

---
+++ StringsPython/ConditionalStatement4.py
@@ -0,0 +1,42 @@
+# # if Statement..
+# age = 21
+
+# if(age >= 18):
+#     print("can vote & apply for license")
+#     print("can drive")
+
+
+#     # elif
+#     light = "green"
+
+#     if(light == "red"):
+#         print("stop")
+#     elif(light == "green"):
+#         print("go")
+#     elif(light == "yellow"):
+#         print("look")
+#         # else
+#     else:
+#         print("light is broken")
+
+
+# 
+        
+
+
+
+
+            # if elif else.
+a = int(input("enter 1st number:"))
+b = int(input("enter 2nd number:"))
+c = int(input("enter 3rd number:"))
+
+if(a >= b and a >= c):
+    print("first number is largest: ", a)
+elif(b >= c):
+    print("second number is largest: ", b)
+else:
+    print("third is largest: ", c) 
+
+
+            
