6657c674c30d36ebcef08a23b7e055383b8dae64
Pradhum80 <kumarroney154@gmail.com>
2026-02-16 18:34:34 +0530

    Tuples : Python

---
+++ StringsPython/Tuples6.py
@@ -0,0 +1,15 @@
+# Tuples :- A built data type that lets us create immutable sequences of values.
+
+tup = (2, 1, 3, 1)
+print(tup[0])
+print(tup[1])
+
+# Slicing
+print(tup[1:4])
+
+# Tuple Methods.
+# tup.index(endx)
+print(tup.index(1))
+
+# count
+print(tup.count(1))
---
+++ StringsPython/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+print(tup[0])
+# print(tup[1])
+
+# # Slicing
+# print(tup[1:4])
