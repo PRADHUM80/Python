34add78bfeee3d35d266e889277db6ac561b133f
Pradhum80 <kumarroney154@gmail.com>
2026-02-17 16:48:37 +0530

    Functions :intro

---
+++ Functions/intro1.py
@@ -0,0 +1,45 @@
+# function..
+# block of Statement that perform a specific task.
+# reduce the code
+
+
+# example..
+
+# def calc_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# calc_sum(2, 3)
+# calc_sum(20, 20)
+# calc_sum(200, 300)
+
+# 2 function definiation
+# def calc_sum(a, b): #parameters
+#     return a + b
+# sum = calc_sum(5 , 5)# function call ; arguments
+# print(sum)
+
+#  3 no parameter no return type
+# def print_hello():
+#     print("Hello World!")
+
+# print_hello()
+# print_hello()
+# print_hello()
+
+# 4 return type
+# def calc_avg(a, b , c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+
+# calc_avg(1, 2, 3)
+
+
+def cal_prod(a,b = 2):
+    print(a * b)
+    return a * b
+
+cal_prod(5)
