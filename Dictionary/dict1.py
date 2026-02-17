27088addc28b4d653faea9c33b266cdf9f2e79ee
Pradhum80 <kumarroney154@gmail.com>
2026-02-17 11:46:02 +0530

    Dictionary : Intro

---
+++ Dictionary/dict1.py
@@ -0,0 +1,16 @@
+# Dictionary :- are used to store data values in Key-value Pairs.
+# They are unordered, mutable, & dont allow duplicate keys.
+
+
+info = {
+    "key" : "value",
+    "name" : "Pradhum",
+    "learning" : "ApnaCollege",
+    "age" : 35,
+    "marks" : 94.4,
+    "Subject" : ["python", "c", "java"], # List
+    "topics" : ("dictionary", "set")
+
+}
+info ["name"]= "Pradhum Kumar"  # add ya modify...
+print(info)
