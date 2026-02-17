500d926183270d574c6f9db781ea8bf123e3e5e0
Pradhum80 <kumarroney154@gmail.com>
2026-02-17 12:59:58 +0530

    For : LOOP

---
+++ LOOPS/ForLoops.py
@@ -0,0 +1,21 @@
+# Loops are used for sequential traversal. for traversing list, string, tuples etc.
+
+# For Loops :- for el in List Some work..
+# nums = [1, 2, 3, 4, 5]
+
+# for val in nums:
+#     print(val)
+
+
+    # 
+    # veg = ["potato", "brinjal", "ladyfingr", "cucumber" ]
+    # for item in veg:
+    #     print(item)
+
+        # tupples..
+
+tup = (1, 2, 3, 4, 5, 6)
+for num in tup:
+    print(num)
+else:
+    print("end")
---
+++ LOOPS/whileloop.py
@@ -0,0 +1,28 @@
+# count = 1
+# while count <= 5 :
+#     print("hello")
+#     count += 1
+#     print(count)
+
+
+#     i = 1
+#     while i <= 5:
+#         print("ApnaCollege")
+#         i+=1
+
+
+# Print numbers from 1 to 5
+i = 1
+while i <= 5:
+    print(i)
+    i+=1
+
+    # continue
+    
+    a = 1
+    while a <= 10:
+        if(a % 2 != 0):
+            a+=1
+            continue #skip
+        print(a)
+        a += 1
