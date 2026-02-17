1dd4c4d8bb2fc030952b11e6cc8b5dc2d26755c9
Pradhum80 <kumarroney154@gmail.com>
2026-02-17 12:15:02 +0530

    Set : intro

---
+++ Set/set1.py
@@ -0,0 +1,17 @@
+# Set
+#  set is the collection of the unorderd items.
+# Each element in the set must be unique & immutable.
+
+collection = {1, 2, 2, 3, 2, 3, 4, "hello", "world", "world",5}
+
+print(collection)
+print(type(collection))
+print(len(collection))  # ignore reapeat values
+
+
+# Or
+
+collection = set() #empty set; syntax
+
+print(type(collection))
+
