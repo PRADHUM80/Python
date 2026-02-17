a93fb614a14f1e4cb0d9e3982a9985a3d42b5e6c
Pradhum80 <kumarroney154@gmail.com>
2026-02-17 13:09:56 +0530

    Pass :Statement

---
+++ LOOPS/PassStatement.py
@@ -0,0 +1,29 @@
+# pass is a null statement that does nothing . it is used as a placeholder for future code.
+
+# for el in range(10):
+# pass
+
+# for i in range(5):
+#     pass
+
+
+# if i > 5:
+#     pass
+# print("Some useful work")
+
+# sum of 5 number
+
+# n = 5
+# sum = 0
+# for i in range (1, n+1):
+#     sum += i
+#     print(sum)
+
+    # factorial number
+n = 5
+fact = 1
+i = 1
+while i <= n:
+    fact *= i
+    i += 1
+    print(fact)
